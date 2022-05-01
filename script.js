95dc1d85ea290b58400beb687e6b58aa052ca319
cloudhum <turbulentbees@gmail.com>
2022-04-30 22:19:29 -0700

    Add buttons and event listeners

--- script.js
+++ script.js
@@ -1,42 +1,58 @@
+
 function computerPlay() {
-//randomly return either 'Rock', 'Paper' or "Scissors" for the computer's move
+    //randomly return either 'Rock', 'Paper' or "Scissors" for the computer's move
     const ROCK = 1;
     const PAPER = 2;
-    const SCISSORS = 3; 
-
+    const SCISSORS = 3;
+    
     //select a random number between 1-3 inclusive
-    let computerMove = Math.floor(Math.random() * 3 + 1); 
-    console.log(computerMove);
+    let computerMove = Math.floor(Math.random() * 3 + 1);
     
     if (computerMove == ROCK) return "rock";
     if (computerMove == PAPER) return "paper";
     if (computerMove == SCISSORS) return "scissors";
 }
 
-function playRound (playerSelection, computerSelection){
-playerSelection = playerSelection.toLowerCase(); 
-console.log(playerSelection);
-
-if (playerSelection == "rock" && computerSelection == "rock") alert("Tie!");
-if (playerSelection == "rock" && computerSelection == "paper") alert("You Lose! Paper beats Rock");
-if (playerSelection == "rock" && computerSelection == "scissors") alert( "You Win! Rock beats Scissors");
-
-if (playerSelection == "paper" && computerSelection == "rock") alert("You Win! Paper beats Rock");
-if (playerSelection == "paper" && computerSelection == "paper") alert("Tie!");
-if (playerSelection == "paper" && computerSelection == "scissors") alert("You Lose! Scissors beats Paper");
-
-if (playerSelection == "scissors" && computerSelection == "rock") alert("You Lose! Rock beats Scissors");
-if (playerSelection == "scissors" && computerSelection == "paper") alert("You Win! Scissors beats Paper");
-if (playerSelection == "scissors" && computerSelection == "scissors") alert("Tie!");
+function playRound(playerSelection) {
+    let computerSelection = computerPlay();
+    console.log("Computer move: " + computerSelection); 
+    
+    if (playerSelection == "rock" && computerSelection == "rock") console.log("Tie!");
+    else if (playerSelection == "rock" && computerSelection == "paper") console.log("You Lose! Paper beats Rock");
+    else if (playerSelection == "rock" && computerSelection == "scissors") console.log("You Win! Rock beats Scissors");
+    
+    else if (playerSelection == "paper" && computerSelection == "rock") console.log("You Win! Paper beats Rock");
+    else if (playerSelection == "paper" && computerSelection == "paper") console.log("Tie!");
+    else if (playerSelection == "paper" && computerSelection == "scissors") console.log("You Lose! Scissors beats Paper");
+    
+    else if (playerSelection == "scissors" && computerSelection == "rock") console.log("You Lose! Rock beats Scissors");
+    else if (playerSelection == "scissors" && computerSelection == "paper") console.log("You Win! Scissors beats Paper");
+    else if (playerSelection == "scissors" && computerSelection == "scissors") console.log("Tie!");
 }
 
-function game(){
-// calls playRound to play a 5 around game that keeps score and reports a winner or loser at the end
-   for (let i = 0; i < 5; i++){
-       playerSelection = prompt("Enter rock, paper, scissors");
-       playRound(playerSelection, computerPlay());
-   }
-}
+const buttonRock = document.createElement('button');
+const buttonPaper = document.createElement('button');
+const buttonScissors = document.createElement('button');
 
-game();
+buttonRock.textContent = "Rock";
+buttonPaper.textContent = "Paper";
+buttonScissors.textContent = "Scissors";
+
+buttonRock.addEventListener('click', function() {
+    playRound("rock");
+});
+buttonPaper.addEventListener('click', function() {
+    playRound("paper");
+});
+buttonScissors.addEventListener('click', function() {
+    playRound("scissors");
+});
+
+const content = document.querySelector('.content');
+content.appendChild(buttonRock);
+content.appendChild(buttonPaper);
+content.appendChild(buttonScissors);
+
+const body = document.querySelector('body');
+body.appendChild(content);
 
